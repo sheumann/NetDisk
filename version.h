11c47d0de3696c0dc820d7f5cffd94c7cdcef657
Stephen Heumann <stephenheumann@gmail.com>
2018-10-08 21:23:00 -0500

    Bump version numbers.

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define USER_AGENT_VERSION "1.0a1"      /* reported in User-Agent string */
-#define BOOT_INFO_VERSION "v1.0a1"      /* displayed during text-mode boot */
-#define DRIVER_VERSION 0x100A           /* GS/OS driver version format */
+#define USER_AGENT_VERSION "1.0b2"      /* reported in User-Agent string */
+#define BOOT_INFO_VERSION "v1.0b2"      /* displayed during text-mode boot */
+#define DRIVER_VERSION 0x100B           /* GS/OS driver version format */
 
