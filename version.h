617c805160f17587575305b53feca5a0ffabaf83
Stephen Heumann <stephenheumann@gmail.com>
2019-07-15 20:26:22 -0500

    Bump version of init and CDev to 1.0.

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define USER_AGENT_VERSION "1.0b3"      /* reported in User-Agent string */
-#define BOOT_INFO_VERSION "v1.0b3"      /* displayed during text-mode boot */
-#define DRIVER_VERSION 0x100B           /* GS/OS driver version format */
+#define USER_AGENT_VERSION "1.0"        /* reported in User-Agent string */
+#define BOOT_INFO_VERSION "v1.0"        /* displayed during text-mode boot */
+#define DRIVER_VERSION 0x1000           /* GS/OS driver version format */
 
